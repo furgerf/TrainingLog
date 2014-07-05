e3aa9559d6a6ec5317de6dc870ee92b152b73f2a
Fabian Furger <mystyfly@gmail.com>
2014-07-05 13:56:28 +0200

    added file

---
+++ TrainingLog/Forms/NewNonSportEntryForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TrainingLog.Forms
+{
+    partial class NewNonSportEntryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.colPicker = new System.Windows.Forms.ColorDialog();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.butOk = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.butCancel = new System.Windows.Forms.Button();
+            this.labColor = new System.Windows.Forms.Label();
+            this.comColorNames = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(76, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(131, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // colPicker
+            // 
+            this.colPicker.SolidColorOnly = true;
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFrom.Location = new System.Drawing.Point(76, 32);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(131, 20);
+            this.dtpFrom.TabIndex = 1;
+            this.dtpFrom.ValueChanged += new System.EventHandler(this.DtpFromValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Start Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "End Date:";
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTo.Location = new System.Drawing.Point(76, 58);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(131, 20);
+            this.dtpTo.TabIndex = 4;
+            this.dtpTo.ValueChanged += new System.EventHandler(this.DtpToValueChanged);
+            // 
+            // butOk
+            // 
+            this.butOk.Location = new System.Drawing.Point(15, 115);
+            this.butOk.Name = "butOk";
+            this.butOk.Size = new System.Drawing.Size(75, 23);
+            this.butOk.TabIndex = 6;
+            this.butOk.Text = "OK";
+            this.butOk.UseVisualStyleBackColor = true;
+            this.butOk.Click += new System.EventHandler(this.ButOkClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Color:";
+            // 
+            // butCancel
+            // 
+            this.butCancel.Location = new System.Drawing.Point(132, 115);
+            this.butCancel.Name = "butCancel";
+            this.butCancel.Size = new System.Drawing.Size(75, 23);
+            this.butCancel.TabIndex = 7;
+            this.butCancel.Text = "Cancel";
+            this.butCancel.UseVisualStyleBackColor = true;
+            this.butCancel.Click += new System.EventHandler(this.ButCancelClick);
+            // 
+            // labColor
+            // 
+            this.labColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labColor.Location = new System.Drawing.Point(165, 84);
+            this.labColor.Name = "labColor";
+            this.labColor.Size = new System.Drawing.Size(42, 20);
+            this.labColor.TabIndex = 8;
+            this.labColor.Click += new System.EventHandler(this.LabColorClick);
+            // 
+            // comColorNames
+            // 
+            this.comColorNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comColorNames.FormattingEnabled = true;
+            this.comColorNames.Location = new System.Drawing.Point(76, 84);
+            this.comColorNames.Name = "comColorNames";
+            this.comColorNames.Size = new System.Drawing.Size(83, 21);
+            this.comColorNames.TabIndex = 5;
+            this.comColorNames.SelectedIndexChanged += new System.EventHandler(this.ComColorNamesTextChanged);
+            // 
+            // NewNonSportEntryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 150);
+            this.Controls.Add(this.comColorNames);
+            this.Controls.Add(this.butCancel);
+            this.Controls.Add(this.labColor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.butOk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.txtName);
+            this.Name = "NewNonSportEntryForm";
+            this.Text = "Non-Sport Entry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ColorDialog colPicker;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Button butOk;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button butCancel;
+        private System.Windows.Forms.Label labColor;
+        private System.Windows.Forms.ComboBox comColorNames;
+    }
+}
