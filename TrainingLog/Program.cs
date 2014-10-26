a0ded768e8d5800a273a5bcb3e6976b0e5ea59fc
mystyfly <mystyfly@gmail.com>
2014-10-26 17:53:03 +0000

    misc stuff

--- TrainingLog/Program.cs
+++ TrainingLog/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Windows.Forms;
-using System.Xml;
-using System.Xml.Serialization;
-using TrainingLog.Entries;
 using TrainingLog.Forms;
 
 namespace TrainingLog
@@ -17,37 +12,9 @@ namespace TrainingLog
         [STAThread]
         private static void Main()
         {
-            //var eq = new List<Equipment>();
-            //eq.Add(new Equipment("Asics Gel Kayano 20", "asics_gel_kayano_20", Common.Sport.Running));
-            //var eqa = eq.ToArray();
-            //var el = new EntryList(eqa);
-
-            //using (var tw = new StreamWriter("foo.xml"))
-            //{
-            //    var ser = new XmlSerializer(typeof(EntryList));
-            //    ser.Serialize(tw, el);
-            //}
-
-            //var serializer = new XmlSerializer(typeof(EntryList));
-            //Equipment[] eq;
-
-            //using (var stringReader = new StringReader(File.ReadAllText("foo.xml")))
-            //using (var reader = XmlReader.Create(stringReader))
-            //{
-            //    eq = ((EntryList)serializer.Deserialize(reader)).EquipmentEntries;
-            //}
-
-            //foreach (var e in eq)
-            //    e.LoadImage();
-
-
-
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             MainForm.Initialize();
-            //new NewEquipmentForm(eq[0]).Show();
             Application.Run();
         }
     }
